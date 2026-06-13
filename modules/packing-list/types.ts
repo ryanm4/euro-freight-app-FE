c28098c256ebc68009dad6e6d749d375efdc525f
chanugatharindu1995 <chanugatharindu1995@gmail.com>
2026-06-13 22:18:34 +0530

    Enhance Packing List Table with additional columns and data structure updates

--- modules/packing-list/types.ts
+++ modules/packing-list/types.ts
@@ -1,6 +1,12 @@
 export interface PACKING_LIST {
-  client_id: number;
-  date: string;
-  created_by: string;
-  po_detail_ids: number[];
+  id: number
+  client_id?: number | null
+  date?: string | null
+  gdn_id?: number | null
+  grn_id?: number | null
+  quantity?: number | null
+  created_by?: string | null
+  created_on?: string | null
+  updated_by?: string | null
+  updated_on?: string | null
 }
